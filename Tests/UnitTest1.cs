68871d2c688c86da0e0fdc1f9020c4646211374b
FenomeN <vlad_simuta@mail.ru>
2021-12-22 13:17:35 +0300

    TestSimple

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -13,14 +13,7 @@ namespace Tests
         [TestMethod]
         public void SimpleDependencyTest()
         {
-            //ismth
-            DependenciesConfiguration configuration = new DependenciesConfiguration();
-
-            configuration.Register<ISmth, ISmthImpl>(LifeTime.Singleton);
-            DependencyProvider provider = new DependencyProvider(configuration);
-
-            ISmthImpl cl = (ISmthImpl)provider.Resolve<ISmth>();
-            Assert.IsNotNull(cl, configuration.ToString() + " " + provider.ToString() + " ");
+           
         }
 
 
